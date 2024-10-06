6ee57971048e4917e1d769b3c737a6e7011e5b0c
ShopFusionTebex <mariusz.karauda@gmail.com>
2024-10-06 12:58:11 +0200

    Add files via upload

---
+++ dl_tanker_mlo/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+this_is_a_map 'yes'
+
+files {
+    'audio/vi_main_game.dat151.rel',
+    'dont_look_pot_plant02.ytyp'
+    }
+data_file 'AUDIO_GAMEDATA' 'audio/vi_main_game.dat'
+data_file 'DLC_ITYP_REQUEST' 'ytyp/dont_look_pot_plant02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'ytyp/dont_look_03c_desklamp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'ytyp/urwis3k_38_barb_plant.ytyp' 
